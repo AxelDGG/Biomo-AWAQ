38d342cad424df96821b49381d8f5b5acda99d8a
Lucypau <a01199124@tec.mx>
2025-09-30 01:57:54 -0600

    Merge pull request #23 from AxelDGG/testVale
    Cambio font, fix home username display

--- app/src/main/java/com/example/awaq1/ui/theme/Theme.kt
+++ app/src/main/java/com/example/awaq1/ui/theme/Theme.kt
@@ -52,7 +52,7 @@ fun AWAQ1Theme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
+        typography = AppTypography,
         content = content
     )
 }
