27c40415e2e0811567d99f8a09d4cdba3dfd9276
Lucypau <a01199124@tec.mx>
2025-10-18 17:15:12 -0600

    ubicación
    Se asegura de si tener permiso de ubicación, para no causar ruido

--- app/src/main/java/com/example/awaq1/data/formularios/Ubicacion.kt
+++ app/src/main/java/com/example/awaq1/data/formularios/Ubicacion.kt
@@ -1,9 +1,11 @@
 package com.example.awaq1.data.formularios
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.Priority
 import com.google.android.gms.tasks.CancellationTokenSource
@@ -11,15 +13,19 @@ import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 
+
 class Ubicacion(private val context: Context) {
 
     private val fusedLocationClient: com.google.android.gms.location.FusedLocationProviderClient =
         com.google.android.gms.location.LocationServices.getFusedLocationProviderClient(context)
 
+    @SuppressLint("MissingPermission")
     suspend fun obtenerCoordenadas(): Pair<Double, Double>? {
 
         return suspendCancellableCoroutine { continuation ->
             val cancellationTokenSource = CancellationTokenSource()
+            if (!hasLocationPermission()) { throw SecurityException("Ubicación no concedida") }
+
             fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, cancellationTokenSource.token)
                 .addOnSuccessListener { location: Location? ->
                     if (location != null) {
