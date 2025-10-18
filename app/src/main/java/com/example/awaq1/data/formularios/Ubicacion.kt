ed2e6b5eb2ec690b6734c3ee38c025c47668125b
Lucypau <a01199124@tec.mx>
2025-10-18 16:49:21 -0600

    ubicación fix

--- app/src/main/java/com/example/awaq1/data/formularios/Ubicacion.kt
+++ app/src/main/java/com/example/awaq1/data/formularios/Ubicacion.kt
@@ -1,25 +1,26 @@
 package com.example.awaq1.data.formularios
 
 import android.Manifest
-import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.Priority
+import com.google.android.gms.tasks.CancellationTokenSource
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
-
 
 class Ubicacion(private val context: Context) {
 
     private val fusedLocationClient: com.google.android.gms.location.FusedLocationProviderClient =
         com.google.android.gms.location.LocationServices.getFusedLocationProviderClient(context)
 
-    @SuppressLint("MissingPermission")
     suspend fun obtenerCoordenadas(): Pair<Double, Double>? {
+
         return suspendCancellableCoroutine { continuation ->
-            fusedLocationClient.lastLocation
+            val cancellationTokenSource = CancellationTokenSource()
+            fusedLocationClient.getCurrentLocation(Priority.PRIORITY_HIGH_ACCURACY, cancellationTokenSource.token)
                 .addOnSuccessListener { location: Location? ->
                     if (location != null) {
                         continuation.resume(Pair(location.latitude, location.longitude))
@@ -30,6 +31,10 @@ class Ubicacion(private val context: Context) {
                 .addOnFailureListener { exception ->
                     continuation.resumeWithException(exception)
                 }
+
+            continuation.invokeOnCancellation {
+                cancellationTokenSource.cancel()
+            }
         }
     }
 
